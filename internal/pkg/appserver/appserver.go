c85120a4ec80081deeeae5169652cf383f8a84d4
Serhii_Kolisnyk <Serhii.Kolisnyk@clarivate.com>
2023-05-26 00:02:14 +0200

    pkg correction

---
+++ internal/pkg/appserver/appserver.go
@@ -0,0 +1,56 @@
+// Server package
+package appserver
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	_ "github.com/Serhii1Epam/simpleHttpServer/internal/pkg/hasher"
+)
+
+type appserver struct {
+	is_runned bool
+}
+
+func (s *appserver) Run() {
+	http.HandleFunc("/exit", handleExit)
+	http.HandleFunc("/about", handleAbout)
+	http.HandleFunc("/", handleIndex)
+
+	s.is_runned = true
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func handleAbout(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL [%s]\n", r.URL.Path)
+	fmt.Fprintf(w, "Method [%v]\n", r.Method)
+	fmt.Fprintf(w, "Simple HTTP Server developed for GO switch program.\n")
+}
+
+func handleExit(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL [%s]\n", r.URL.Path)
+	fmt.Fprintf(w, "Method [%v]\n", r.Method)
+	fmt.Fprintf(w, "Closing Simple HTTP Server...\n")
+	os.Exit(1)
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Process [%v] method...\n", r.Method)
+	switch r.Method {
+	case "GET":
+		{
+
+		}
+	case "POST":
+		{
+			fmt.Fprintf(w, "We shouldn't be here [%v]. Continue...\n", r.Method)
+		}
+	default:
+		{
+			fmt.Fprintf(w, "Server can't handle method [%v]. Continue...\n", r.Method)
+		}
+	}
+}
+
