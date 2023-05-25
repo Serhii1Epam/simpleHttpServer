e0c77bc8f44a530b4a5b0d376788684c24a4ef28
Serhii_Kolisnyk <Serhii.Kolisnyk@clarivate.com>
2023-05-25 23:39:52 +0200

    pkg correction

--- cmd/smplHttpSrv/main.go
+++ cmd/smplHttpSrv/main.go
@@ -7,15 +7,12 @@ import (
 	"fmt"
 	"log"
 
-	_ "github.com/Serhii1Epam/simpleHttpServer/internal/pkg/server"
+	_ "github.com/Serhii1Epam/simpleHttpServer/internal/pkg/appServer"
 )
 
 func main() {
 	fmt.Println("Simple HTTP Server start...")
 	defer fmt.Println("Simple HTTP Server finish work.")
-	s := NewServer()
-
-	//http.HandleFunc("/about", aboutHandler)
-	//http.HandleFunc("/", indexHandler)
-	log.Fatal(s.routes())
+	srv := appServer{false}
+	log.Fatal(srv.Run())
 }
