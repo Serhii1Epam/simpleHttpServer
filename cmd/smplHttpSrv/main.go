8756c2c85db83815217f86b5696034c213b140ca
Serhii_Kolisnyk <Serhii.Kolisnyk@clarivate.com>
2023-05-25 21:20:11 +0200

    package location change

--- cmd/smplHttpSrv/main.go
+++ cmd/smplHttpSrv/main.go
@@ -1,21 +1,51 @@
 /*
- * Start Simple HTTP server
+ * Cmd start Simple HTTP server
  */
 package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
-func main() {
-	i := 0
-	fmt.Println("Simple HTTP Server start...")
-	for {
-		i++
-		if i == 1000 {
-			fmt.Printf("i = %v\n", i)
-			break
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Page[%s]\n", r.URL.Path)
+	fmt.Fprintf(w, "Method[%v]\n", r.Method)
+	fmt.Fprintf(w, "Simple HTTP Server developed for GO switch program.\n")
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Process [%v] method...\n", r.Method)
+	switch r.Method {
+	case "GET":
+		{
+
+		}
+	case "DELETE":
+		{
+
+		}
+	case "UPDATE":
+		{
+
+		}
+	case "PUT":
+		{
+
+		}
+	default:
+		{
+			fmt.Fprintf(w, "Server can't handle method [%v]. Continue...\n", r.Method)
 		}
 	}
-	fmt.Println("Simple HTTP Server finish work.")
+}
+
+func main() {
+	fmt.Println("Simple HTTP Server start...")
+	defer fmt.Println("Simple HTTP Server finish work.")
+
+	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/", indexHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
